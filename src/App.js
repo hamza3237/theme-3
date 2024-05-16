d378a0c4000ad13c7701ce36707cfe9da064c218
Hamzaali0088 <154756967+Hamzaali0088@users.noreply.github.com>
2024-05-15 23:51:13 -0700

    remove product

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from '../src/components/Navbar';
-import Product from './pages/product/Product'
 import Home from './pages/home/Home'
 import SingleProduct from './pages/singleproduct/SingleProduct'
 import Footer from './components/Footer'
@@ -13,7 +12,6 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/products" element={<Product/>} />
           <Route path="/productdetail" element={<SingleProduct />} />
         </Routes>
       </main>
